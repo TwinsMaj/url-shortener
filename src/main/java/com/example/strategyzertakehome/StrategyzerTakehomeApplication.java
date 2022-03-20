5931481c7988174c63efa6f36c4b37d13dbc2a82
Taiwo Maja <taiwom@klluster.com>
2022-03-20 12:23:48 +0200

    chore(init): initialize project

---
+++ src/main/java/com/example/strategyzertakehome/StrategyzerTakehomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.strategyzertakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyzerTakehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyzerTakehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/strategyzertakehome/StrategyzerTakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strategyzertakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyzerTakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
