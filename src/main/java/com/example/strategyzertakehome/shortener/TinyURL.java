981af64e916c7b8563593ab871999a98bc33e5e0
Taiwo Maja <taiwom@klluster.com>
2022-03-20 12:53:47 +0200

    chore(TinyURL): add TinyURL MongoDb entity

---
+++ src/main/java/com/example/strategyzertakehome/shortener/TinyURL.java
@@ -0,0 +1,32 @@
+package com.example.strategyzertakehome.shortener;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TinyURL implements Serializable {
+    @Id
+    private String id;
+    private String encodedURL;
+    private String shortURL;
+    private String bigURL;
+    private LocalDateTime created;
+    private LocalDateTime expiry;
+
+    public TinyURL(String encodedURL, String shortURL, String bigURL, LocalDateTime created, LocalDateTime expiry) {
+        this.encodedURL = encodedURL;
+        this.shortURL = shortURL;
+        this.bigURL = bigURL;
+        this.created = created;
+        this.expiry = expiry;
+    }
+}
